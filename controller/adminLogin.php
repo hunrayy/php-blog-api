7693c7dbccf60bd9e3fbbbfbb27b60221b834cfc
hunrayy <henryokiyi8@gmail.com>
2024-07-12 18:31:20 +0100

    added redirecting

--- controller/adminLogin.php
+++ controller/adminLogin.php
@@ -22,9 +22,9 @@
 
             if(mysqli_num_rows($result) > 0) {
                 $admin_user = mysqli_fetch_assoc($result);
-                echo json_encode($admin_user);
+                // echo json_encode($admin_user);
         
-                echo "Welcome admin $email";
+                echo "Login successful, Welcome admin $email";
                 //  $active_user = json_encode($user['firstname']);
                 // echo $active_user;
                 // session_start();
@@ -51,56 +51,4 @@
 
 
     }
-    // require("config.php");
-    // function login($email, $password){
-    //     global $_conn;
-    //     mysqli_real_escape_string($_conn, trim($email));
-    //     mysqli_real_escape_string($_conn, trim($password));
-    //     if(empty($email) || empty($password)){
-    //         echo "invalid user info...email and password required";
-    //         $empty = "email and password is required";
-    //         return json_encode([
-    //             "message" => "user info issue",
-    //             "error" => $empty
-    //         ]);
-    //     }
-    //     try{
-    //         $sql = "SELECT * FROM `admins` WHERE `email` = '$email' AND `password` = SHA('$password')";
-    //         echo $sql;
-            
-    //         $result = mysqli_query($_conn, $sql);
 
-    //         if(mysqli_num_rows($result) == 1) {
-    //             $admin_user = mysqli_fetch_assoc($result);
-    //             // echo json_encode($admin_user);
-    //             $role_id = json_decode(json_encode($admin_user))->role_id;
-    //             if($role_id == 1){
-    //                 echo "Welcome super admin $email";
-    //                 // $active_user = json_encode($admin_user['firstname']);
-    //                 // echo $active_user;
-    //                 session_start();
-    //                 // if($_SESSION['user']['active_user'] = $active_user){
-    //                 //     header("location: adminDashboard/dashboard.php");
-    //                 // }
-
-    //                 // return json_encode([
-    //                 //     "message" => "welcome $email"
-    //                 // ]);
-    //             }else if($role_id !== 1){
-    //                 echo "Welcome admin $email";
-    //                 return json_encode([
-    //                     "message" => "welcome admin $email"
-    //                 ]);
-    //             } 
-    //         }
-    //     }catch(Exception $e){
-    //         echo "not an admin $e";
-    //         return json_encode([
-    //             "message" => "unauthorized access",
-    //             "error" => "not an admin"
-    //         ]);
-    //     }
-
-
-    // }
-?>
